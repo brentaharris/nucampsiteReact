d0bda3a9c1dd0c417c9e251a6f5b851218c46a2e
brentaharris <brentaharris@users.noreply.github.com>
2022-05-30 20:30:56 -0400

    added json db for development

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import "./App.css";
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import AboutPage from './pages/AboutPage';
+import AboutPage from "./pages/AboutPage";
+
+// start json server: json-server -H 0.0.0.0 --watch db.json -p 3001 -d 2000
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='contact' element={<ContactPage />}/>
-        <Route path='directory' element={<CampsitesDirectoryPage />}/>
-        <Route path='/about' element={<AboutPage />} />
-        <Route 
-          path='directory/:campsiteId' 
-          element={<CampsiteDetailPage />}
-        />
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
       </Routes>
       <Footer />
     </div>
