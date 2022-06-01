28b661b80a44fefc017fd02812bdac053463f8a9
brentaharris <brentaustinharris@gmail.com>
2022-06-01 19:04:19 -0400

    testing json server

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Footer from "./components/Footer";
 import AboutPage from "./pages/AboutPage";
 
 // start json server: json-server -H 0.0.0.0 --watch db.json -p 3001 -d 2000
+// 
 
 function App() {
   const dispatch = useDispatch();
